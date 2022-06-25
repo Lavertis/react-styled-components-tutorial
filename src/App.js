00c7f3cfab449162349546fea32f0063335a56f0
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-06-25 20:19:40 +0200

    Add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import { Container } from "./components/styles/Container.styled";
+import GlobalStyles from "./components/styles/Global";
+import content from "./Content";
+import Footer from "./components/Footer";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "768px",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <Container>
+        {content.map((item, index) => {
+          return (
+            <Card key={index} item={item}>
+              {item.title}
+            </Card>
+          );
+        })}
+      </Container>
+      <Footer />
+    </ThemeProvider>
   );
 }
 
